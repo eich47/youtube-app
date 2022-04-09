496b89574deed1cb3a2e5adee5a5e12456be745e
eich <eich@tut.by>
2022-04-09 11:21:50 +0300

    вывел катрочки

--- my-first-project/src/app/search/search-item/search-item.component.ts
+++ my-first-project/src/app/search/search-item/search-item.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SearchItemModel } from '../search-item.model';
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
-  styleUrls: ['./search-item.component.scss']
+  styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent implements OnInit {
+export class SearchItemComponent {
 
-  constructor() { }
+  @Input() video: SearchItemModel = {
+    kind: '',
+    etag: '',
+    id: '',
+    snippet: {
+      publishedAt: '',
+      channelId: '',
+      title: '',
+      description: '',
+      thumbnails: {
+        default: {
+          url: '',
+          width: 0,
+          height: 0,
+        },
+        medium: {
+          url: '',
+          width: 0,
+          height: 0,
+        },
+        high: {
+          url: '',
+          width: 0,
+          height: 0,
+        },
+        standard: {
+          url: '',
+          width: 0,
+          height: 0,
+        },
+        maxres: {
+          url: '',
+          width: 0,
+          height: 0,
+        },
+      },
+      channelTitle: '',
+      tags: [],
+      categoryId: '',
+      liveBroadcastContent: '',
+      defaultLanguage: undefined,
+      localized: {
+        title: '',
+        description: '',
+      },
+      defaultAudioLanguage: '',
+    },
+    statistics: {
+      viewCount: '',
+      likeCount: '',
+      dislikeCount: '',
+      favoriteCount: '',
+      commentCount: '',
+    },
+  };
 
-  ngOnInit(): void {
-  }
+
+
 
 }
